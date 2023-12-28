15b23204b79ebc5ddb539ab2403608588d882472
yf <uskfrtt@gmail.com>
2023-12-28 19:03:10 +0900

    commit through makefile

--- philo/test.sh
+++ philo/test.sh
@@ -6,21 +6,37 @@
 # 4. time_to_sleep
 # 5. number_of_times_each_philosopher_must_eat
 
-ARGS1="./philo \
-2147483647 \
-21479p83647 \
-2147483647 \
-2147483647 \
-tur&"
+ARGV1="200"
+ARGV2="800"
+ARGV3="100"
+ARGV4="3000"
+ARGV5="100"
+# ARGV3="100@"
+# ARGV3="opqij&"
 
-ARGS2="./philo \
-200 \
-800 \
-100 \
-100 \
-"
+# ARGV1="2147483647"
+# ARGV2="2147483647"
+# ARGV3="2147483647"
+# ARGV4="2147483647"
+# ARGV5="2147483647"
+
+# ARGS1="./philo \
+# 2147483647 \
+# 21479p83647 \
+# 2147483647 \
+# 2147483647 \
+# tur&"
+
+# ARGS2="./philo \
+# 200 \
+# 800 \
+# 100 \
+# 100 \
+# 3000"
 
 # echo ${ARGS1}
 # $ARGS1
-echo ${ARGS2}
-$ARGS2
+CMD="./philo $ARGV1 $ARGV2 $ARGV3 $ARGV4 $ARGV5"
+
+echo ${CMD}
+$CMD
